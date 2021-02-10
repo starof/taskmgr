fd41ed9cf2572178026cbb16219883ac52584f0f
starof <925309723@qq.com>
2021-02-10 08:05:03 +0800

    menu button work well

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,24 +4,26 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent, FooterComponent, SidebarComponent
   ]
 })
-export class CoreModule { 
-  constructor(@Optional() @SkipSelf() parent: CoreModule){
-    if(parent){
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
       throw new Error('模块以及存在，不能再次价值！');
     }
   }
- 
+
 }
