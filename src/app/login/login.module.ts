b7b59a086cd638cdff8fc97745b5cbd9b2ec9b1e
starof <925309723@qq.com>
2021-02-11 08:09:47 +0800

    generate register

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,11 @@ import { SharedModule } from './../shared/shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     SharedModule,
     LoginRoutingModule
