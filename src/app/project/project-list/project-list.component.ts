0400111bb3a2d9be16ad9b5e8ce98fae993b255f
starof <925309723@qq.com>
2021-02-12 20:53:54 +0800

    project list

--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -12,4 +12,8 @@ export class ProjectListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openNewProjectDialog(){
+    
+  }
+
 }
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
 import { NewProjectComponent } from './new-project/new-project.component';
 import { InviteComponent } from './invite/invite.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [ProjectListComponent, ProjectItemComponent, NewProjectComponent, InviteComponent],
   imports: [
-    CommonModule
+    SharedModule,
+
   ]
 })
 export class ProjectModule { }
