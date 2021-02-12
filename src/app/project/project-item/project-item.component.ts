9f1753f698eef994401934dc830e7d7c35576db4
starof <925309723@qq.com>
2021-02-12 21:09:16 +0800

    show project list and project item

--- src/app/project/project-item/project-item.component.ts
+++ src/app/project/project-item/project-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project-item',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-item.component.scss']
 })
 export class ProjectItemComponent implements OnInit {
-
+  @Input() item: any;
   constructor() { }
 
   ngOnInit(): void {
   }
+  onEditClick(){
 
+  }
+  onInviteClick(){
+
+  }
+  onDeleteClick(){
+
+  }
 }
--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
-
+  projects = [
+    {
+      id:1,
+      name: "企业协作平台",
+      desc: "这是一个企业内部项目",
+      coverImg: "assets/images/covers/0.jpg"
+    },
+    {
+      id:2,
+      name: "自动化测试项目",
+      desc: "这是一个企业内部项目",
+      coverImg: "assets/images/covers/1.jpg"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
