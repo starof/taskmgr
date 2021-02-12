046d359250848c3918647e69708618d81f5525b1
starof <925309723@qq.com>
2021-02-12 18:20:53 +0800

    generate project components

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { InviteComponent } from './invite/invite.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProjectListComponent, ProjectItemComponent, NewProjectComponent, InviteComponent],
   imports: [
     CommonModule
   ]
