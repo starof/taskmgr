ae2db07a49c7ac8da90bd5748c73773badd38bcf
starof <925309723@qq.com>
2021-02-14 08:17:25 +0800

    add proprity redio buttons in new task component

--- src/app/task/new-task/new-task.component.ts
+++ src/app/task/new-task/new-task.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-task.component.scss']
 })
 export class NewTaskComponent implements OnInit {
-
+  priorities = [
+    {
+      label:'紧急',
+      value:1
+    },
+    {
+      label:'紧急',
+      value:1
+    },
+    {
+      label:'紧急',
+      value:1
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
