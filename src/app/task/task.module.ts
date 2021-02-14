10568fba2ede5414857e239aab6b8938e622687b
starof <925309723@qq.com>
2021-02-14 08:12:23 +0800

    generate new task component

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -5,11 +5,12 @@ import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskHeaderComponent } from './task-header/task-header.component';
 import { SharedModule } from '../shared/shared.module';
 import { TaskRoutingModule } from './task-routing.module';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 
 
 @NgModule({
-  declarations: [TaskHomeComponent, TaskListComponent, TaskItemComponent, TaskHeaderComponent],
+  declarations: [TaskHomeComponent, TaskListComponent, TaskItemComponent, TaskHeaderComponent, NewTaskComponent],
   imports: [
     SharedModule,
     TaskRoutingModule
