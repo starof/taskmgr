a5465cc26790778dffdac8d0afe9c47039f7ccd8
starof <925309723@qq.com>
2021-02-14 16:46:32 +0800

    ease-in ease-out example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   animations: [
     trigger('square', [
-      state('green', style({ 'background-color': 'green', 'height': '100px', 'transform': 'translateX(0)' })),
-      state('red', style({ 'background-color': 'red', 'height': '50px', 'transform': 'translateX(100%)' })),
-      transition('green=>red', animate('.2s 1s')),//动画持续的时间，延迟多久开始
-      transition('red=>green', animate(1000)),
+      state('green', style({ 'background-color': 'green','height':'100px','transform':'translateY(-100%)'  })),
+      state('red', style({ 'background-color': 'red','height':'100px' ,'transform':'translateY(100%)'})),
+      transition('green=>red', animate('.8s ease-in')),
+      transition('red=>green', animate('.8s ease-out')),
     ])
   ]
 })
