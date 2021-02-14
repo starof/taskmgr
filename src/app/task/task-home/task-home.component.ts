c822291b3a2720a6b4fa302d0a056caae51f0faf
starof <925309723@qq.com>
2021-02-14 17:33:15 +0800

    add route animate to tash home component

--- src/app/task/task-home/task-home.component.ts
+++ src/app/task/task-home/task-home.component.ts
@@ -1,5 +1,6 @@
+import { slideToRight } from './../../animate/router.animate';
 import { CopyTaskComponent } from './../copy-task/copy-task.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NewTaskComponent } from '../new-task/new-task.component';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
@@ -8,10 +9,11 @@ import { NewTaskListComponent } from '../new-task-list/new-task-list.component';
 @Component({
   selector: 'app-task-home',
   templateUrl: './task-home.component.html',
-  styleUrls: ['./task-home.component.scss']
+  styleUrls: ['./task-home.component.scss'],
+  animations: [slideToRight]
 })
 export class TaskHomeComponent implements OnInit {
-
+  @HostBinding('@routeAnim') state: any;
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
@@ -55,7 +57,7 @@ export class TaskHomeComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
 
-  launchNewListDialog(){
+  launchNewListDialog() {
     const dialogRef = this.dialog.open(NewTaskListComponent, {
       data: {
         title: '新建任务列表'
@@ -63,7 +65,7 @@ export class TaskHomeComponent implements OnInit {
     });
     dialogRef.afterClosed().subscribe(result => console.log(result));
   }
-  launchEditListDialog(){
+  launchEditListDialog() {
     const dialogRef = this.dialog.open(NewTaskListComponent, {
       data: {
         title: '更改列表名称'
