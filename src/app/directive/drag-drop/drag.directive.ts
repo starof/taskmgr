e82e5a6536892624da80c0af0c10840bc11343cc
starof <925309723@qq.com>
2021-02-15 12:57:01 +0800

    update drag directive

--- src/app/directive/drag-drop/drag.directive.ts
+++ src/app/directive/drag-drop/drag.directive.ts
@@ -1,3 +1,4 @@
+import { DragDropService } from './../drag-drop.service';
 import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
@@ -18,13 +19,20 @@ export class DragDirective {
   @Input()
   draggedClass: string = '';
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  //多定义了一个dragTag
+  @Input()
+  dragTag: string = '';
+  @Input()
+  dragData: any;//给DragDropService传递的数据
+
+  constructor(private el: ElementRef, private renderer: Renderer2, private service: DragDropService) { }
 
   @HostListener('dragstart', ['$event'])
   ondragstart(event: Event) {
     //判断drag元素是不是指令应用的元素发起的
     if (this.el.nativeElement === event.target) {
       this.renderer.addClass(this.el.nativeElement, this.draggedClass); //给el增加一个class
+      this.service.setDragData({ tag: this.dragTag, data: this.dragData });// 给service传递上数据
     }
   }
 
