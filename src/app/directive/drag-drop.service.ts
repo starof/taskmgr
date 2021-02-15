84871a89a9b636da9f933637a384f39331f3d30e
starof <925309723@qq.com>
2021-02-15 12:43:37 +0800

    update drag-drop service

--- src/app/directive/drag-drop.service.ts
+++ src/app/directive/drag-drop.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+//数据结构
+export interface DragData {
+  tag: string,//多重拖拽的话，是哪一级拖拽，用户自己包装唯一性，不能重复
+  data: any //传递的数据
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DragDropService {
+  //用BehaviorSubject总能记住上一次的值
+  private _dragData = new BehaviorSubject<DragData | null>(null);
+
+  setDragData(data: DragData) {
+    this._dragData.next(data);
+  }
+
+  getDragData() {
+    this._dragData.asObservable();
+  }
+
+  clearDragData() {
+    this._dragData.next(null);
+  }
 
   constructor() { }
 }
