1a17aee8702c8375495d2fdbdb41ab1e7e580120
starof <925309723@qq.com>
2021-02-18 07:33:45 +0800

    formGroup: formControl

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl("wang@163.com", Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl("",Validators.required),
+    })
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit(form: FormGroup, event: Event) {
+    event.preventDefault();
+    console.log(JSON.stringify(form.value));
+    console.log(form.valid);
   }
 
 }
