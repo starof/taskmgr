7e538b8d048e1f67ee4f327839d824d154a664cb
starof <925309723@qq.com>
2021-02-21 16:30:47 +0800

    add pipe debug operator

--- src/app/services/quote.service.ts
+++ src/app/services/quote.service.ts
@@ -3,15 +3,16 @@ import { Quote } from './../domain/quote.model';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { debug } from '../utils/debug.util';
 
 @Injectable({
     providedIn: 'root'
 })
 export class QuoteService {
-    constructor(@Inject('BASE_CONFIG') private config: any, private http: HttpClient) { }
+    constructor(@Inject('BASE_CONFIG') private config: any, private httpClient: HttpClient) { }
 
     getQuote(): Observable<Quote> {
         const uri = `${this.config.uri}/quotes/${Math.floor(Math.random() * 10)}`;
-        return this.http.get(uri).pipe(map(quote => quote as Quote))
+        return this.httpClient.get(uri).pipe(debug('quote: '), map(quote => quote as Quote))
     }
 }
