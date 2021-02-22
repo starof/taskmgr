5a27997be64a1cfe1384ef317167f1d815b58c5b
starof <925309723@qq.com>
2021-02-22 08:03:24 +0800

    template UI and component ControlValueAccessor skeleton

--- src/app/shared/age-input/age-input.component.ts
+++ src/app/shared/age-input/age-input.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-age-input',
   templateUrl: './age-input.component.html',
-  styleUrls: ['./age-input.component.scss']
+  styleUrls: ['./age-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => AgeInputComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => AgeInputComponent),
+      multi: true
+    }
+  ]
 })
-export class AgeInputComponent implements OnInit {
+export class AgeInputComponent implements ControlValueAccessor {
 
   constructor() { }
+  private propagateChange = (_: any) => { };
+
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
