bd2bc9d1dc5367e5712b989f23e3fa878066c136
starof <925309723@qq.com>
2021-02-23 08:48:53 +0800

    update project service

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,9 +1,58 @@
-import { Injectable } from '@angular/core';
+import { Project } from './../domain';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+  private readonly domain = 'projects';
+  private headers = new Headers({
+    'Content-type': 'application/json'
+  });
+  constructor(private httpClient: HttpClient, @Inject('BASE_CONFIG') private config: any) { }
 
-  constructor() { }
+  //POST
+  add(project: Project): Observable<Project> {
+    project.id = undefined;
+    const uri = `${this.config.uri}/${this.domain}`;
+    return this.httpClient.post(uri, JSON.stringify(project)).pipe(
+      map(res => res as Project)
+    )
+  }
+
+  //PUT/patch
+  update(project: Project): Observable<Project> {
+    const uri = `${this.config.uri}/${this.domain}/${project.id}`;
+    const toUpdate = {
+      name: project.name,
+      desc: project.desc,
+      coverImg: project.coverImg
+    }
+    return this.httpClient.patch(uri, JSON.stringify(toUpdate)).pipe(
+      map(res => res as Project)
+    )
+  }
+
+  //DELETE
+  delete(project: Project): Observable<Project> {
+    project.id = undefined;
+    const uri = `${this.config.uri}/${this.domain}`;
+    return this.httpClient.post(uri, JSON.stringify(project)).pipe(
+      map(res => res as Project)
+    )
+  }
+
+  //GET
+  get(userId: string): Observable<Project[]> {
+    const uri = `${this.config.uri}/${this.domain}`;
+    return this.httpClient.get(uri, { params: { 'members_like': userId } }).pipe(
+      map(res => res as Project[])
+    )
+  }
+
+
+
 }
