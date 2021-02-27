4015afbabe48e2664dc170b8ed1e17de9054f4d6
starof <925309723@qq.com>
2021-02-27 23:03:58 +0800

    update auth service

--- src/app/domain/user.model.ts
+++ src/app/domain/user.model.ts
@@ -1,6 +1,6 @@
 export interface User {
   id?: string;
-  // email: string;
+  email: string;
   name?: string;
   password?: string;
   avatar?: string;
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,39 +1,62 @@
+import { User } from './../domain/user.model';
+import { Auth } from './../domain/auth.model';
 import { Project } from './../domain';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { count, map, mapTo, mergeMap, switchMap } from 'rxjs/operators';
-import { from, Observable } from 'rxjs';
+import { from, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly domain = 'projects';
+  private readonly domain = 'users';
   private headers = new HttpHeaders({
     'Content-type': 'application/json'
   });
+  private token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' +
+    '.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9' +
+    '.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
+
   constructor(private httpClient: HttpClient, @Inject('BASE_CONFIG') private config: any) { }
 
+
   //注册
-  register(project: Project): Observable<Project> {
-    project.id = undefined;
+  register(user: User): Observable<Auth> {
     const uri = `${this.config.uri}/${this.domain}`;
-    return this.httpClient.post<Project>(uri, JSON.stringify(project), { headers: this.headers }).pipe(
-      map(res => res as Project)
-    )
+    return this.httpClient
+      .get(uri, { params: { 'email': user.email } })
+      .pipe(
+        switchMap(res => {
+          if ((<User[]>res).length > 0) {
+            return throwError('username existed');
+          }
+          return this.httpClient
+            .post(uri, JSON.stringify(user), { headers: this.headers })
+            .pipe(map(r => ({ token: this.token, user: <User>r })));
+        })
+      )
+
   }
 
   //登录
-  login(project: Project): Observable<Project> {
-    const uri = `${this.config.uri}/${this.domain}/${project.id}`;
-    const toUpdate = {
-      name: project.name,
-      desc: project.desc,
-      coverImg: project.coverImg
-    }
-    return this.httpClient.patch(uri, JSON.stringify(toUpdate), { headers: this.headers }).pipe(
-      map(res => res as Project)
+  login(username: string, password: string): Observable<Auth> {
+    const uri = `${this.config.uri}/${this.domain}`;
+    return this.httpClient.get(uri,
+      { params: { 'email': username, 'password': password } }
     )
+      .pipe(
+        map(res => {
+          const users = <User[]>res;
+          if (users.length === 0) {
+            throw new Error('Username or password incorrect');
+          }
+          return {
+            token: this.token,
+            user: users[0]
+          }
+        }))
   }
 
 }
