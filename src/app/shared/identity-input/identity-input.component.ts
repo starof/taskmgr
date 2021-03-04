69703e836759f6162c451bbc0fcc55cce9979d6b
starof <925309723@qq.com>
2021-03-05 07:39:11 +0800

    IdentityInputCcomponent implement ControlValueAccessor

--- src/app/shared/identity-input/identity-input.component.ts
+++ src/app/shared/identity-input/identity-input.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, forwardRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-identity-input',
   templateUrl: './identity-input.component.html',
-  styleUrls: ['./identity-input.component.scss']
+  styleUrls: ['./identity-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => IdentityInputComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => IdentityInputComponent),
+      multi: true
+    }
+  ]
 })
-export class IdentityInputComponent implements OnInit {
+export class IdentityInputComponent implements OnInit, ControlValueAccessor {
 
   constructor() { }
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
